465e2fc523535cf9fcb5e3b2085242c718cd8c37
Tinashe <marvellous.chimboza@gmail.com>
2023-05-19 18:45:30 +0100

    Initial commit

--- src/main/java/SetsAndHashSets.java
+++ src/main/java/SetsAndHashSets.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class SetsAndHashSets {
     public static void main(String[] args) {
 
-        Set<String> names = new HashSet<>();
+        //Set<String> names = new HashSet<>();
+        Set<String> names = new TreeSet<>(); //Hashset is fast, treeSet is OK for ordering
+        //there is also LinkedHashSet - this one maintains insertion order
+        //real world we will use HashSet
 
         /**
          *  whenever you don't want duplicates but don't care about the order, e.g removing duplicates elements from a list
